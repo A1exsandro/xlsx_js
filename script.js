773cbe187501daef801224610b4f71bb07aa6316
A1exsandro <alexsandro.acp@gmail.com>
2022-09-24 14:32:15 -0400

    test with json

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ const xlsx = require("xlsx");
 
 const wb = xlsx.readFile("alunosNow.xlsx");
 
-console.log(wb.SheetNames);
+const ws = wb.Sheets["AlunosNow"];
+
+const data = xlsx.utils.sheet_to_json(ws);
+
+console.log(data);
